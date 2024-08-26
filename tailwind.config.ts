b14ee5149d04d45b1cf77628f737345eaa611469
ema <ema@MacBook-Pro-di-Emanuele.local>
2024-08-26 03:35:53 -0700

    update on dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
-  darkMode: 'class', 
+  darkMode: 'false',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
