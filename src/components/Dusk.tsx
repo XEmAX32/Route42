0f768be5d321139a4474df307333ec40c36fcc1b
Emanuele Sacco <ema.sacco32@gmail.com>
2024-10-31 01:17:10 +0100

    fixes

--- src/components/Dusk.tsx
+++ src/components/Dusk.tsx
@@ -1,10 +1,12 @@
+import { Suspense } from "react";
 import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
     <Spline
       scene="https://prod.spline.design/jN9VUulDUPIpIpQO/scene.splinecode" 
-      className="w-[90vw] md:w-[50vw] md:h-[50vh] md:mx-auto"
+      style={{ width: "100vw", height: "60vh" }}
+      // className="w-[90vw] md:w-[50vw] md:h-[50vh] md:mx-auto"
     />
   );
 }
