bb91242a9277ebc1ae4acb76d47f6571f248ec72
Emanuele Sacco <ema.sacco32@gmail.com>
2024-11-10 03:06:38 +0100

    added alt

--- src/app/crepuscolo/page.tsx
+++ src/app/crepuscolo/page.tsx
@@ -6,7 +6,7 @@ export default function Crepuscolo() {
   return (
     <main className="flex min-h-screen flex-col align-start bg-black">
       <div className="relative">
-        <Image src="/background.png" width="992" height="614" style={{ width: "100%" }} />
+        <Image src="/background.png" width="992" height="614" alt="background" style={{ width: "100%" }} />
         <div className="background-gradient"></div>
         <div className="absolute inset-0 top-0 flex flex-col items-center justify-center">
           <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
